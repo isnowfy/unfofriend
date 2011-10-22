df4cfa2e7047dbc5254797f8de602bc71a11d356
isnowfy <isnowfy@gmail.com>
2011-10-23 00:53:04 +0800

    add diff.py

--- auth.py
+++ auth.py
@@ -10,54 +10,54 @@ consumer_secret = 'VQtFQ8dKEW4waEbwGBeFcWg6ZDpKKuPZvowLr9qSpTQ'
 domain = 'https://unfofriend.appspot.com/'
 
 class OAuthTokenDB(db.Model):
-  oakey = db.StringProperty()
-  secret = db.StringProperty()
+    oakey = db.StringProperty()
+    secret = db.StringProperty()
 
 def getauth():
-  if not users.get_current_user():return None
-  user_name = users.get_current_user()
-  auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-  oauth_token = OAuthTokenDB.get_by_key_name(key_names='_%s' % user_name)
-  if oauth_token is None:return None
-  auth.set_access_token(oauth_token.oakey, oauth_token.secret)
-  return auth
+    if not users.get_current_user():return None
+    user_name = users.get_current_user()
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    oauth_token = OAuthTokenDB.get_by_key_name(key_names='_%s' % user_name)
+    if oauth_token is None:return None
+    auth.set_access_token(oauth_token.oakey, oauth_token.secret)
+    return auth
 
 class OAuth(webapp.RequestHandler):
-  def get(self):
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret, domain + 'callback')
-    try:
-      redirect_url = auth.get_authorization_url()
-    except tweepy.TweepError:
-      self.response.out.write('Error! Failed to get request token.')
-      return
-    user_name=users.get_current_user()
-    oauth_token = OAuthTokenDB(key_name='_%s' % user_name, oakey=auth.request_token.key, secret=auth.request_token.secret)
-    oauth_token.put()
-    self.redirect(redirect_url)
+    def get(self):
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret, domain + 'callback')
+        try:
+            redirect_url = auth.get_authorization_url()
+        except tweepy.TweepError:
+            self.response.out.write('Error! Failed to get request token.')
+            return
+        user_name=users.get_current_user()
+        oauth_token = OAuthTokenDB(key_name='_%s' % user_name, oakey=auth.request_token.key, secret=auth.request_token.secret)
+        oauth_token.put()
+        self.redirect(redirect_url)
 
 class OAuthCallback(webapp.RequestHandler):
-  def get(self):
-    self.response.out.write('here0')
-    verifier = self.request.get('oauth_verifier')
-    self.response.out.write('here1')
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    user_name=users.get_current_user()
-    oauth_token = OAuthTokenDB.get_by_key_name(key_names='_%s' % user_name)
-    auth.set_request_token(oauth_token.oakey, oauth_token.secret)
-    try:
-      auth.get_access_token(verifier)
-    except tweepy.TweepError:
-      self.response.out.write('Error! Failed to get access token.')
-      return
-    user_name = users.get_current_user()
-    oauth_token = OAuthTokenDB(key_name='_%s' % user_name, oakey=auth.access_token.key, secret=auth.access_token.secret)
-    oauth_token.put()
-    self.redirect('/')
+    def get(self):
+        self.response.out.write('here0')
+        verifier = self.request.get('oauth_verifier')
+        self.response.out.write('here1')
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        user_name=users.get_current_user()
+        oauth_token = OAuthTokenDB.get_by_key_name(key_names='_%s' % user_name)
+        auth.set_request_token(oauth_token.oakey, oauth_token.secret)
+        try:
+            auth.get_access_token(verifier)
+        except tweepy.TweepError:
+            self.response.out.write('Error! Failed to get access token.')
+            return
+        user_name = users.get_current_user()
+        oauth_token = OAuthTokenDB(key_name='_%s' % user_name, oakey=auth.access_token.key, secret=auth.access_token.secret)
+        oauth_token.put()
+        self.redirect('/')
 
 class OAuthLogout(webapp.RequestHandler):
-  def get(self):
-    user_name = users.get_current_user()
-    oauth_token = OAuthTokenDB.get_by_key_name(key_names='_%s' % user_name)
-    if oauth_token:
-      db.delete(oauth_token)
-    self.redirect('/')
+    def get(self):
+        user_name = users.get_current_user()
+        oauth_token = OAuthTokenDB.get_by_key_name(key_names='_%s' % user_name)
+        if oauth_token:
+            db.delete(oauth_token)
+        self.redirect('/')
